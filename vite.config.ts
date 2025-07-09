4a8a838713319504a823c43a01a8fe10f43f32c0
timtskov <timtskov@gmail.com>
2025-07-09 11:13:29 +0300

    Update config and deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project.real/', // ⬅️ See on oluline!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
